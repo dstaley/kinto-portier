656645e225e3a4d69ffca3464118caf714d1d388
Dylan Staley <88163+dstaley@users.noreply.github.com>
2020-07-05 15:16:26 -0700

    add support for refreshing a session ttl

--- kinto_portier/__init__.py
+++ kinto_portier/__init__.py
@@ -11,6 +11,7 @@ __version__ = pkg_resources.get_distribution(__package__).version
 DEFAULT_SETTINGS = {
     'portier.cache_ttl_seconds': 5 * 60,
     'portier.session_ttl_seconds':  15 * 60,
+    'portier.refresh_session_ttl': False,
     'portier.heartbeat_timeout_seconds': 3,
     'portier.broker_uri': "https://broker.portier.io",
     'portier.requested_scope': 'openid email',
